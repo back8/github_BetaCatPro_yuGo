276edf97044c48756bea2e4b29da3cbdf0905d85
ZJin <32411994+BetaCatPro@users.noreply.github.com>
2019-03-21 13:55:19 +0800

    Update settings.py

--- yuGou/settings.py
+++ yuGou/settings.py
@@ -136,7 +136,7 @@ EMAIL_PORT = 25
 # 发送邮件的邮箱
 EMAIL_HOST_USER = 'zj20162325@163.com'
 # 在邮箱中设置的客户端授权密码
-EMAIL_HOST_PASSWORD = 'by66717151623'
+EMAIL_HOST_PASSWORD = '这是密码'
 # 收件人看到的发件人
 EMAIL_FROM = '愉购<zj20162325@163.com>'
 
